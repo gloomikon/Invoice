4f33fb57c1078115d70120f859cee02e9571951f
gloomikon <gloomikon@gmail.com>
2025-07-19 18:07:28 +0200

    Main screen empty state UI

--- Invoice/Sources/Modules/Main/MainModule.swift
+++ Invoice/Sources/Modules/Main/MainModule.swift
@@ -6,14 +6,8 @@ struct MainModule: Module {
     let router: MainRouter
 
     func build() -> UIViewController {
-        @Injected var database: DatabaseManager
-        let viewModel = MainViewModel(
-            router: router
-        )
-        let view = MainView(
-            viewModel: viewModel,
-            databaseManager: database
-        )
+        let viewModel = MainViewModel(router: router)
+        let view = MainView(viewModel: viewModel)
         let viewController = HostingController(rootView: view)
         return viewController
     }
--- Invoice/Sources/Modules/Main/MainViewModel.swift
+++ Invoice/Sources/Modules/Main/MainViewModel.swift
@@ -1,11 +1,69 @@
 import Combine
 import Depin
+import Foundation
+
+// TODO: - Remove
+struct Invoice {
+
+    enum PaidStatus {
+        case unpaid
+        case paid
+    }
+    let id: UUID
+    let title: String
+    let date: Date
+    let sum: Double
+    let dueDays: Int
+    let status: PaidStatus
+}
 
 class MainViewModel: ObservableObject {
 
     // MARK: - Injected properties
 
     @Injected private var appStorage: AppStorage
+    @Injected var databaseManager: DatabaseManager
+
+//    let invoices: [Invoice] = [
+//        Invoice(
+//            id: UUID(),
+//            title: "Website redesign",
+//            date: Date(),
+//            sum: 4500,
+//            dueDays: 10,
+//            status: .unpaid
+//        ),
+//        Invoice(
+//            id: UUID(),
+//            title: "Logo creation",
+//            date: Date(),
+//            sum: 1600,
+//            dueDays: 14,
+//            status: .unpaid
+//        ),
+//        Invoice(
+//            id: UUID(),
+//            title: "Animation",
+//            date: Date(),
+//            sum: 4500,
+//            dueDays: 17,
+//            status: .paid
+//        ),
+//        Invoice(
+//            id: UUID(),
+//            title: "gloomikon test",
+//            date: Date(),
+//            sum: 4500,
+//            dueDays: 2,
+//            status: .paid
+//        )
+//    ]
+
+    let invoices: [Invoice] = []
+
+    var total: Double {
+        invoices.map(\.sum).reduce(0, +)
+    }
 
     private let router: MainRouter
 
