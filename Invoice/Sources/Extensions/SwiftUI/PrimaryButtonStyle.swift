c928525c6eefc9cd1e5b8cdc6469297bc2125f65
DmytroYashchenko <2795901@gmail.com>
2025-07-22 22:34:04 +0300

    Update PrimaryButtonStyle to handle inactive state

--- Invoice/Sources/Extensions/SwiftUI/PrimaryButtonStyle.swift
+++ Invoice/Sources/Extensions/SwiftUI/PrimaryButtonStyle.swift
@@ -2,15 +2,18 @@ import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
 
+    @Environment(\.isEnabled) private var enabled
+
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.poppins(size: 16, weight: .semiBold))
             .frame(maxWidth: .infinity)
             .frame(height: 56)
-            .background(.linkedIn, in: .capsule)
-            .foregroundStyle(.white)
+            .background(.linkedIn.opacity(enabled ? 1 : 0.5), in: .capsule)
+            .foregroundStyle(.white.opacity(enabled ? 1 : 0.5))
             .scaleEffect(configuration.isPressed ? 0.975 : 1)
             .animation(.easeInOut(duration: 0.2), value: configuration.isPressed)
+            .animation(.easeInOut(duration: 0.2), value: enabled)
     }
 }
 
